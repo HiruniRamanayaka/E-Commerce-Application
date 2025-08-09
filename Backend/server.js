1ae1440a90fcd5eac3f61c39357244fdd43a5aa3
Hiruni Ramanayaka <hiruniramanayaka9@gmail.com>
2025-08-09 22:34:33 +0530

    Created connection with mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,15 @@ const PORT = 5000;
 
 app.use(cors());
 
+//MongoDB connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+        console.error("MongoDB connection error: ", err.message);
+        process.exit(1);
+    })
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
