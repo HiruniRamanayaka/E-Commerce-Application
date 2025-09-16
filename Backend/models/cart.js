08091cc535f64c02922591d818c78e10d9d4e377
Hiruni Ramanayaka <hiruniramanayaka9@gmail.com>
2025-09-17 02:38:25 +0530

    Created Models for Cart and Orders

---
+++ Backend/models/cart.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true, min: 1, default: 1 },
+    selectedSize: {
+      size: { type: String },
+      price: { type: Number }
+    }
+  },
+  { _id: false }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [cartItemSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
